95e99f4eed51cf43b0fe3d2dd1ef679722bce139
Luke S <steimelluke@gmail.com>
2023-05-23 21:05:10 -0400

    ip auth proxy

--- src/undetected_chromedriver/proxyplugin.py
+++ src/undetected_chromedriver/proxyplugin.py
@@ -54,17 +54,47 @@ class ProxyExtension:
     );
     """
 
-    def __init__(self, host, port, user, password):
+    background_js_no_proxy = """
+    var config = {
+        mode: "fixed_servers",
+        rules: {
+            singleProxy: {
+                scheme: "http",
+                host: "%s",
+                port: %s
+            },
+            bypassList: ["localhost"]
+        }
+    };
+
+    chrome.proxy.settings.set({value: config, scope: "regular"}, function() {});
+
+    chrome.webRequest.onAuthRequired.addListener(
+        callbackFn,
+        { urls: ["<all_urls>"] },
+        ['blocking']
+    );
+    """
+
+    def __init__(self, host, port, user=None, password=None):
         self._dir = os.path.normpath(tempfile.mkdtemp())
 
         manifest_file = os.path.join(self._dir, "manifest.json")
+
         with open(manifest_file, mode="w") as f:
             f.write(self.manifest_json)
 
-        background_js = self.background_js % (host, port, user, password)
-        background_file = os.path.join(self._dir, "background.js")
-        with open(background_file, mode="w") as f:
-            f.write(background_js)
+
+        if user is None:
+            background_js = self.background_js_no_proxy % (host, port, user, password)
+            background_file = os.path.join(self._dir, "background.js")
+            with open(background_file, mode="w") as f:
+                f.write(background_js)
+        else: 
+            background_js = self.background_js % (host, port, user, password)
+            background_file = os.path.join(self._dir, "background.js")
+            with open(background_file, mode="w") as f:
+                f.write(background_js)
 
     @property
     def directory(self):
